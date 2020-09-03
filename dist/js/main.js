1d45135efbba582565737a2a7a98b7ee107918b3
hainam0412 <hainam041294@gmail.com>
2020-09-04 00:07:40 +0700

    fix side bar

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,3 +3,26 @@ $(".has-sub").click(function () {
 
   $(this).find(".sub-menu").slideToggle(300);
 });
+
+const shows = document.querySelectorAll(".show-menu");
+for (let show of shows) {
+  show.addEventListener("click", (q) => {
+    q.preventDefault();
+    const p = show.parentElement;
+    const ul = p.querySelector("ul");
+    if (ul.style.display === "none") {
+      ul.style.display = "block";
+      ul.style.animation = `bounceUp .5s both`;
+    }
+  });
+}
+
+const backs = document.querySelectorAll(".back");
+for (let back of backs) {
+  back.addEventListener("click", (e) => {
+    e.preventDefault();
+    const p = back.parentElement;
+    p.style.display = "none";
+    p.style.animation = "";
+  });
+}
