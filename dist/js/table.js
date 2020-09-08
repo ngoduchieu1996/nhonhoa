3f2ee9bbe364aac5e7038aadd720a19b4a886f2c
hainam0412 <hainam041294@gmail.com>
2020-09-08 08:56:47 +0700

    push

---
+++ dist/js/table.js
@@ -0,0 +1,32 @@
+function getSum() {
+  const tableChiTietHoaDon = document.querySelector(".table-chitiethoadon");
+
+  if (tableChiTietHoaDon) {
+    const moneys = document.querySelectorAll("tr.row td:last-child");
+    const mArr = [];
+
+    for (let m of moneys) {
+      mArr.push(m.innerHTML);
+    }
+
+    const arr = mArr.map(convertCurrencyToNum);
+
+    function convertCurrencyToNum(a) {
+      return Number(a.replace(/\./g, ""));
+    }
+
+    function convertNumToCurrency(a) {
+      return a.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    }
+
+    const sumE = tableChiTietHoaDon.querySelector(".sum");
+
+    sum = arr.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+
+    sumE.innerHTML = convertNumToCurrency(sum);
+  }
+}
+
+getSum();
